2a78684e2e06fdf7c3d5e5641e4db25488720d47
Younes Baali <unessbaali@gmail.com>
2022-05-26 20:31:40 +0200

    New commit message

--- src/screens/Sampler.js
+++ src/screens/Sampler.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity} from 'react-native';
+import { Audio } from 'expo-av';
 
 
 const Sampler = ({currentSong, songs}) => {
-    const test = "hellddo kffhona";
+    
+    const [sound, setSound] = useState();
+
+    async function playSound() {
+        
+        console.log('Loading Sound');
+        const { sound } = await Audio.Sound.createAsync(
+            
+           require('../samples/clap_2.wav')
+
+        );
+        setSound(sound);
+    
+        console.log('Playing Sound');
+        await sound.playAsync(); }
+    
+      useEffect(() => {
+        return sound
+          ? () => {
+              console.log('Unloading Sound');
+              sound.unloadAsync(); }
+          : undefined;
+      }, [sound]);
 
     return(
 
-        <View>
-            <Text>{test}</Text>
-        </View> 
+    <View style ={styles.container}>
+
+
+        <TouchableOpacity onPress={playSound}>
+            <ImageBackground source={{uri:currentSong.cover}}  style={styles.image}>
+            </ImageBackground>
+        </TouchableOpacity>
+
+    </View> 
     )
 }
 
 
+const styles = StyleSheet.create({
+    container:{
+        resizeMode: "cover",
+    },
+
+    image:{
+    width: 50,
+    height: 50,
+    padding: 10,
+    borderRadius: 70,
+    justifyContent: "center"
+
+    }
+})
 export default Sampler;
