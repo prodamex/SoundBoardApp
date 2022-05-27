7d9981c0ad331d7356e190ab3803bfd137d889bf
Younes Baali <unessbaali@gmail.com>
2022-05-27 21:10:42 +0200

    error when passing props

--- src/screens/data.js
+++ src/screens/data.js
@@ -11,7 +11,7 @@ function data() {
     {
         cover:
           "https://thumbs.dreamstime.com/b/clap-hands-colored-line-icon-simple-yellow-brown-element-illustration-clap-hands-concept-outline-symbol-design-emoji-clap-128886962.jpg",
-        audio: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3",
+        audio: "./samples/clap_1.wav/",
         id: uuidv4(),
         active: true,
       },
